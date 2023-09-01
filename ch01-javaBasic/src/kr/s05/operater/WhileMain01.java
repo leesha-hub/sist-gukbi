9bb4803ea182f3644b9408510b998e7c0dd9e7fa
leesha1223 <leesha1223@naver.com>
2023-09-01 23:52:31 +0900

    20230901

---
+++ ch01-javaBasic/src/kr/s05/operater/WhileMain01.java
@@ -0,0 +1,22 @@
+package kr.s05.operater;
+
+public class WhileMain01 {
+	public static void main(String[] args) {
+		//while문은 조건 비교에 만족할 때만 수행문을 수행하는 반복문
+		int i = 1;//초기값
+		
+		     //조건식
+		while(i <= 10) {
+			System.out.println(i++);//증감식
+		}
+		System.out.println("----------------");
+		
+		int j = 10;//초기값
+		while(j >= 0) {
+			System.out.println(j--);
+		}
+	}
+}
+
+
+
