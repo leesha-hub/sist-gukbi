169165356d55dbda99a71311464de47446ab446e
leesha1223 <leesha1223@naver.com>
2023-10-12 09:00:14 +0900

    20231011

---
+++ ch03-javaJDBC/src/kr/s11/book/BookAdminMain.java
@@ -0,0 +1,84 @@
+package kr.s11.book;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BookAdminMain {
+	private BufferedReader br;
+	private BookDAO dao;
+	
+	public BookAdminMain() {
+		try {
+			br = new BufferedReader(new InputStreamReader(System.in));
+			dao = new BookDAO();
+			//메뉴 호출
+			callMenu();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			if(br!=null)try {br.close();}catch(IOException e) {}
+		}
+	}
+	//메뉴
+	public void callMenu() throws IOException{
+		while(true) {
+			System.out.print("1.도서 등록,2.도서 목록,3.대출 목록,4.회원 목록,5.종료>");
+			try {
+				int no = Integer.parseInt(br.readLine());
+				if(no == 1) {//도서 등록
+					/*
+					도서명:자바
+					카테고리:IT
+					1건의 도서 정보를 등록했습니다.
+					 */
+				}else if(no == 2) {//도서 목록
+					/*
+					---------------------------
+					번호	카테고리	도서명	대출여부	등록일
+					-------------------------------------
+					42	천문		별이야기	대출가능	2023-10-10
+					41	IT		자바		대출중	2023-10-10
+					---------------------------
+					 */
+				}else if(no == 3) {//대출 목록
+					/*
+					--------------------------
+					번호	대출여부	대출자id	대출도서명	대출일		반납일
+					----------------------------------------
+					20	대출		blue	자바		2023-10-10
+					19	반납		sky		별이야기	2023-10-09	2023-10-10
+					--------------------------
+					 */
+				}else if(no == 4) {//회원 목록
+					/*
+					------------------------------
+					아이디	이름		전화번호			가입일
+					------------------------------
+					sky		홍길동	010-1234-5678	2023-09-09
+					blue	박영식	010-5678-1234	2023-09-08
+					------------------------------
+					 */
+					
+				}else if(no == 5) {//종료
+					System.out.println("프로그램 종료");
+					break;
+				}else {
+					System.out.println("잘못 입력했습니다.");
+				}
+			}catch(NumberFormatException e) {
+				System.out.println("[숫자만 입력 가능]");
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		new BookAdminMain();
+	}
+}
+
+
+
+
+
+
---
+++ ch03-javaJDBC/src/kr/s11/book/table2.sql
@@ -0,0 +1,32 @@
+create table member(
+ me_id varchar2(10) primary key,
+ me_passwd varchar2(10) not null,
+ me_name varchar2(30) not null,
+ me_phone varchar2(13) not null,
+ me_regdate date default sysdate not null
+);
+
+create table book(
+ bk_num number primary key,
+ bk_name varchar2(90) not null,
+ bk_category varchar2(30) not null,
+ bk_regdate date default sysdate not null
+);
+create sequence book_seq;
+
+create table reservation(
+ re_num number primary key,
+ re_status number(1) not null, -- 0.반납(미대출),1.대출중
+ bk_num number not null references book (bk_num),
+ me_id varchar2(10) not null references member (me_id),
+ re_regdate date default sysdate not null,
+ re_modifydate date
+);
+create sequence reservation_seq;
+
+
+
+
+
+
+
