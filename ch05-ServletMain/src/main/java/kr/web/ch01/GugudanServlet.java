f49f4b6e80f61ecb8952c9157fb24aaf87070588
leesha1223 <leesha1223@naver.com>
2023-11-03 08:32:22 +0900

    20231102

---
+++ ch05-ServletMain/src/main/java/kr/web/ch01/GugudanServlet.java
@@ -0,0 +1,18 @@
+package kr.web.ch01;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/gugudan")
+public class GugudanServlet extends HttpServlet{
+	public void doGet(HttpServletRequest request,
+			         HttpServletResponse resposne)
+	                            throws ServletException,IOException{
+		
+	}
+}
---
+++ ch05-ServletMain/src/main/java/kr/web/ch01/NowServlet.java
@@ -0,0 +1,43 @@
+package kr.web.ch01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//주소 지정
+@WebServlet("/now")
+public class NowServlet extends HttpServlet{
+	public void doGet(HttpServletRequest request,
+			          HttpServletResponse response)
+	                    throws ServletException,IOException{
+		Date now = new Date();
+		SimpleDateFormat sf = 
+				new SimpleDateFormat("yyyy년MM월dd일 a hh:mm:ss");
+		
+		//문서 타입 및 캐릭터셋 지정
+		response.setContentType("text/html;charset=utf-8");
+		
+		//HTML 출력을 위한 출력 스트림 생성
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head><title>현재 날짜와 시간</title></head>");
+		out.println("<body>");
+		out.println("현재 날짜와 시간 : " + sf.format(now));
+		out.println("</body>");
+		out.println("</html>");
+		out.close();
+		
+	}
+}
+
+
+
+
+
