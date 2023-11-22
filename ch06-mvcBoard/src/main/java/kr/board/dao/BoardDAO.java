f8e6bd31312ec2e0bf9248d81682d582f991f09b
leesha1223 <leesha1223@naver.com>
2023-11-23 08:56:41 +0900

    20231122

--- ch06-mvcBoard/src/main/java/kr/board/dao/BoardDAO.java
+++ ch06-mvcBoard/src/main/java/kr/board/dao/BoardDAO.java
@@ -183,6 +183,29 @@ public class BoardDAO {
 		}
 	}
 	//글 삭제
+	public void delete(int num)throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = null;
+		
+		try {
+			//커넥션풀로부터 커넥션 할당
+			conn = DBUtil.getConnection();
+			//SQL문 작성
+			sql = "DELETE FROM svboard WHERE num=?";
+			//PreparedStatement 객체 생성
+			pstmt = conn.prepareStatement(sql);
+			//?에 데이터 바인딩
+			pstmt.setInt(1, num);
+			//SQL문 실행
+			pstmt.executeUpdate();
+			
+		}catch(Exception e) {
+			throw new Exception(e);
+		}finally {
+			DBUtil.executeClose(null, pstmt, conn);
+		}
+	}
 	
 }
 
