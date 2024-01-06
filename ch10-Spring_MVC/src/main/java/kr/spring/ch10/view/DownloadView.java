9bd0daac4f0347310df10bf340b840d9bb34a1a5
leesha1223 <leesha1223@naver.com>
2024-01-06 11:41:49 +0900

    20240105

--- ch10-Spring_MVC/src/main/java/kr/spring/ch10/view/DownloadView.java
+++ ch10-Spring_MVC/src/main/java/kr/spring/ch10/view/DownloadView.java
@@ -1,11 +1,15 @@
 package kr.spring.ch10.view;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.servlet.view.AbstractView;
 
 public class DownloadView extends AbstractView{
@@ -32,8 +36,20 @@ public class DownloadView extends AbstractView{
 				file.getName().getBytes("utf-8"),"iso-8859-1");
 		
 		response.setHeader("Content-Disposition", 
-				         "attachment; filename=\"file.txt\";");
+				         "attachment; filename=\""+fileName+"\";");
+		response.setHeader("Content-Transfer-Encoding", "binary");
 		
+		OutputStream out = response.getOutputStream();
+		FileInputStream fis = null;
+		try {
+			fis = new FileInputStream(file);
+			FileCopyUtils.copy(fis, out);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			if(fis!=null)try {fis.close();}catch(IOException e) {}
+		}
+		out.flush();
 	}
 
 }
