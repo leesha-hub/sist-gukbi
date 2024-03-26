855d80b51bc2277efb51148107585095aaee0b86
leesha1223 <leesha1223@naver.com>
2024-03-26 13:38:47 +0900

    3번

--- ch01-javaBasic/src/CodingTest.java
+++ ch01-javaBasic/src/CodingTest.java
@@ -1,22 +1,30 @@
 public class CodingTest {
 	public static void main(String[] args) {
-		int[] arr = {1,2,3};
-//		int[] arr = {1,2,3,4};
-		
+		int a = 10;
+
 		Solution sl = new Solution();
-		sl.solution(arr);
+		sl.solution(a);
 	}
 }
 
 class Solution {
-    public int solution(int[] arr) {
-    	int answer = 0;
-    	int[] positiveIntArr = arr;
-    	for(int i=0; i<=positiveIntArr.length; i++) {
-    		for(int j=i+1; j<positiveIntArr.length; j++) {
-    			answer += positiveIntArr[i] * positiveIntArr[j];
-    		}
-    	}
-    	return answer;
-    }
+	public int solution(int a) {
+		int answer = 0;
+		int number = a;
+
+		int divideCount = 0;
+		for (int i = 2; i <= a; i++) {
+			if(isPrime(i)) {
+				answer += i;
+			}
+		}
+		return answer;
+	}
+	
+	private boolean isPrime(int num) {
+		for (int i = 2; i * i <= num; i++) {
+	        if (num % i == 0) return false; 
+	    }
+	    return true; 
+	}
 }
