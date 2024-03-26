06184e9aad66db550c7d2169b9451322663ca434
leesha1223 <leesha1223@naver.com>
2024-03-26 13:37:54 +0900

    2번

--- ch01-javaBasic/src/CodingTest.java
+++ ch01-javaBasic/src/CodingTest.java
@@ -1,19 +1,7 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class CodingTest {
 	public static void main(String[] args) {
-		String[][] arr = {
-				{ "answer : 1", "score : 5"},
-				{ "answer : 3", "score : 3"},
-				{ "answer : 2", "score : 4"},
-				
-//				{ "answer : 1", "score : 5"},
-//				{ "answer : 5", "score : 5"},
-//				{ "answer : 2", "score : 4"},
-				};
+		int[] arr = {1,2,3};
+//		int[] arr = {1,2,3,4};
 		
 		Solution sl = new Solution();
 		sl.solution(arr);
@@ -21,42 +9,14 @@ public class CodingTest {
 }
 
 class Solution {
-    public List<String> solution(String[][] arr) {
-        String[][] answerSheet = arr; 
-    	HashMap<String, Integer> scores = new HashMap<>();
-        scores.put("a", 0);
-        scores.put("b", 0);
-        scores.put("c", 0);
-        
-    	for (String[] strings : answerSheet) {
-			int arrAnswer = Integer.parseInt(strings[0].substring(strings[0].length() - 1));
-			int arrScore = Integer.parseInt(strings[1].substring(strings[1].length() - 1));
-			
-			if(arrAnswer == 1) {
-				scores.put("a", (scores.get("a") + arrScore));
-			} else if (arrAnswer == 3) {
-				scores.put("b", (scores.get("b") + arrScore));
-			} else if (arrAnswer == 5) {
-				scores.put("c", (scores.get("c") + arrScore));
-			}
-    	}
-    	
-    	List<String> topScorers = new ArrayList<>();
-    	int maxScore = 0;
-    	for(Map.Entry<String, Integer> entry : scores.entrySet()) {
-    		if(entry.getValue() > maxScore) {
-    			topScorers.clear();
-    			topScorers.add(entry.getKey());
-    			maxScore = entry.getValue();
-    		} else if(entry.getValue() == maxScore) {
-    			topScorers.add(entry.getKey());
+    public int solution(int[] arr) {
+    	int answer = 0;
+    	int[] positiveIntArr = arr;
+    	for(int i=0; i<=positiveIntArr.length; i++) {
+    		for(int j=i+1; j<positiveIntArr.length; j++) {
+    			answer += positiveIntArr[i] * positiveIntArr[j];
     		}
     	}
-    	
-    	List<String> answer = new ArrayList<>();
-    	for (String topScorer : topScorers) {
-    		answer.add(topScorer + " : " + maxScore);
-        }
     	return answer;
     }
 }
