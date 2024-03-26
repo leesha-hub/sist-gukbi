f93f9ce1df93d7d08c442ff6237704a3f515e16b
leesha1223 <leesha1223@naver.com>
2024-03-26 21:03:48 +0900

    8번

--- ch01-javaBasic/src/CodingTest.java
+++ ch01-javaBasic/src/CodingTest.java
@@ -1,40 +1,24 @@
-import java.util.HashMap;
-import java.util.Map;
-
 public class CodingTest {
 	public static void main(String[] args) {
-//		String[] participant = { "트리케라톱스", "티라노사우루스", "바리오닉스"};
-//		String[] completion = { "바리오닉스", "티라노사우루스" };
-		String[] participant = { "티라노사우루스", "파키케팔로사우루스", "티라노사우루스", "스테고사우루스"};
-		String[] completion = { "파키케팔로사우루스", "스테고사우루스", "티라노사우루스" };
+		int[] number = {1,2,3,4};
 		Solution sl = new Solution();
-		sl.solution(participant, completion);
+		sl.solution(number);
 	}
 }
 
 class Solution {
-	public String[] solution(String[] participant, String[] completion) {
-		Map<String, Integer> safeDinosaur = new HashMap<String, Integer>();
-		for (String dinosaur : participant) {
-			Integer dinoCount = safeDinosaur.get(dinosaur);
-			if (dinoCount != null) {
-				safeDinosaur.put(dinosaur, dinoCount + 1);
-			} else {
-				safeDinosaur.put(dinosaur, 1);
-			}
-		}
-
-		for (String dinosaur : completion) {
-			Integer dinoCount = safeDinosaur.get(dinosaur);
-			if (dinoCount != null) {
-				if (dinoCount == 1) {
-					safeDinosaur.remove(dinosaur);
-				} else {
-					safeDinosaur.put(dinosaur, dinoCount - 1);
-				}
-			}
-		}
-		String[] answer = safeDinosaur.keySet().stream().toArray(String[]::new);
+	public int solution(int[] number) {
+		int[] arr = number;
+		int answer = 0;
+		answer = factorial(arr.length);
+		System.out.println(answer);
 		return answer;
+	}
+	
+	public int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n * factorial(n - 1);
 	}
 }
