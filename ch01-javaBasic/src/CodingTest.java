f74708c5bb91c28274589fdeb746e6db2c775863
leesha1223 <leesha1223@naver.com>
2024-03-26 20:30:24 +0900

    9번

--- ch01-javaBasic/src/CodingTest.java
+++ ch01-javaBasic/src/CodingTest.java
@@ -1,30 +1,40 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class CodingTest {
 	public static void main(String[] args) {
-		int a = 10;
-
+//		String[] participant = { "트리케라톱스", "티라노사우루스", "바리오닉스"};
+//		String[] completion = { "바리오닉스", "티라노사우루스" };
+		String[] participant = { "티라노사우루스", "파키케팔로사우루스", "티라노사우루스", "스테고사우루스"};
+		String[] completion = { "파키케팔로사우루스", "스테고사우루스", "티라노사우루스" };
 		Solution sl = new Solution();
-		sl.solution(a);
+		sl.solution(participant, completion);
 	}
 }
 
 class Solution {
-	public int solution(int a) {
-		int answer = 0;
-		int number = a;
-
-		int divideCount = 0;
-		for (int i = 2; i <= a; i++) {
-			if(isPrime(i)) {
-				answer += i;
+	public String[] solution(String[] participant, String[] completion) {
+		Map<String, Integer> safeDinosaur = new HashMap<String, Integer>();
+		for (String dinosaur : participant) {
+			Integer dinoCount = safeDinosaur.get(dinosaur);
+			if (dinoCount != null) {
+				safeDinosaur.put(dinosaur, dinoCount + 1);
+			} else {
+				safeDinosaur.put(dinosaur, 1);
 			}
 		}
+
+		for (String dinosaur : completion) {
+			Integer dinoCount = safeDinosaur.get(dinosaur);
+			if (dinoCount != null) {
+				if (dinoCount == 1) {
+					safeDinosaur.remove(dinosaur);
+				} else {
+					safeDinosaur.put(dinosaur, dinoCount - 1);
+				}
+			}
+		}
+		String[] answer = safeDinosaur.keySet().stream().toArray(String[]::new);
 		return answer;
-	}
-	
-	private boolean isPrime(int num) {
-		for (int i = 2; i * i <= num; i++) {
-	        if (num % i == 0) return false; 
-	    }
-	    return true; 
 	}
 }
