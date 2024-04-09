51d0598e2b16e8e400f89f94e94d01df2fcb1ab2
leesha1223 <leesha1223@naver.com>
2024-04-09 22:39:33 +0900

    코테

--- ch01-javaBasic/src/CodingTest.java
+++ ch01-javaBasic/src/CodingTest.java
@@ -1,24 +1,61 @@
 public class CodingTest {
 	public static void main(String[] args) {
-		int[] number = {1,2,3,4};
-		Solution sl = new Solution();
-		sl.solution(number);
+		int a[] = { 69, 10, 30, 2, 16, 8, 31, 22 };
+		int size = a.length;
+		Sort S = new Sort();
+		System.out.printf("\n 정렬할 원소 : ");
+		for (int i = 0; i < a.length; i++) {
+			System.out.printf("  %d", a[i]);
+		}
+		System.out.println();
+		S.mergeSort(a, 0, size - 1);
 	}
 }
 
-class Solution {
-	public int solution(int[] number) {
-		int[] arr = number;
-		int answer = 0;
-		answer = factorial(arr.length);
-		System.out.println(answer);
-		return answer;
+class Sort {
+	private int sorted[] = new int[30];
+
+	public void merge(int a[], int m, int middle, int n) {
+		int size = a.length;
+		int i, j, k, t;
+		i = m;
+		j = middle + 1;
+		k = m;
+
+		while (i <= middle && j <= n) {
+			if (a[i] <= a[j])
+				sorted[k] = a[i++];
+			else
+				sorted[k] = a[j++];
+			k++;
+		}
+		if (i > middle) {
+			for (t = j; t <= n; t++, k++) {
+				sorted[k] = a[t];
+			}
+		} else {
+			for (t = i; t <= middle; t++, k++) {
+				sorted[k] = a[t];
+			}
+		}
+
+		for (t = m; t <= n; t++) {
+			a[t] = sorted[t];
+		}
+		System.out.printf("\n 병합 정렬 >> ");
+		for (t = 0; t < size; t++) {
+			System.out.printf("%3d", a[t]);
+		}
+
 	}
-	
-	public int factorial(int n) {
-        if (n == 0) {
-            return 1;
-        }
-        return n * factorial(n - 1);
+
+	public void mergeSort(int a[], int m, int n) {
+		int middle;
+		if (m < n) {
+			middle = (m + n) / 2;
+			mergeSort(a, m, middle);
+			mergeSort(a, middle + 1, n);
+			merge(a, m, middle, n);
+		}
 	}
 }
--- ch01-javaBasic/src/dangit3.java
+++ ch01-javaBasic/src/dangit3.java
@@ -1,115 +1,73 @@
-//import java.util.Arrays;
-//import java.util.Calendar;
-//import java.util.LinkedHashMap;
-//import java.util.Map;
-//import java.util.Map.Entry;
-//
-///*
-//과제는 시작하기로 한 시각이 되면 시작합니다.
-//새로운 과제를 시작할 시각이 되었을 때, 
-//
-//기존에 진행 중이던 과제가 있다면 진행 중이던 과제를 멈추고 
-//새로운 과제를 시작합니다.
-//
-//진행중이던 과제를 끝냈을 때, 잠시 멈춘 과제가 있다면, 
-//멈춰둔 과제를 이어서 진행합니다.
-//
-//만약, 과제를 끝낸 시각에 새로 시작해야 되는 과제와 
-//잠시 멈춰둔 과제가 모두 있다면, 새로 시작해야 하는 과제부터 진행합니다.
-//
-//멈춰둔 과제가 여러 개일 경우, 가장 최근에 멈춘 과제부터 시작합니다.
-//plans	result
-//[["korean", "11:40", "30"], ["english", "12:10", "20"], ["math", "12:30", "40"]]	["korean", "english", "math"]
-//[["science", "12:40", "50"], ["music", "12:20", "40"], ["history", "14:00", "30"], ["computer", "12:30", "100"]]	["science", "history", "computer", "music"]
-//[["aaa", "12:00", "20"], ["bbb", "12:10", "30"], ["ccc", "12:40", "10"]]	["bbb", "ccc", "aaa"]
-//*/
-//
-//public class dangit3 {
-//	public static void main(String[] args) {
-//		String[][] plans = {
-////				{ "korean", "11:40", "30" }, 
-////				{ "english", "12:10", "20" }, 
-////				{ "math", "12:30", "40" } }; 
-//				
-//				{ "music", "12:20", "40" }, { "computer", "12:30", "100" }, { "science", "12:40", "50" },
-//				{ "history", "14:00", "30" }, };
-////				
-////				{"aaa", "12:00", "20"},
-////				{"bbb", "12:10", "30"},
-////				{"ccc", "12:40", "10"} };
-//		Solution ss = new Solution();
-//		ss.solution(plans);
-//	}
-//}
-//
-//import java.util.Arrays;
-//import java.util.Calendar;
-//import java.util.LinkedHashMap;
-//import java.util.Map;
-//import java.util.Map.Entry;
-//
-//class Solution {
-//	public String[] solution(String[][] plans) {
-//		String[] answer = new String[plans.length];
-//
-//		// plans[x][1] 기준으로 정렬
-//		Arrays.sort(plans, (o1, o2) -> o1[1].compareTo(o2[1]));
-//
-//		String[] planFinishSubject = new String[plans.length];
-//		Map<String, Integer> planLeftSubject = new LinkedHashMap<String, Integer>();
-//		
-//		int finishCount = 0; // answer index
-//		long difference = 0; // 앞/뒤 시간차
-//		int planLeftTime = 0; // 강의를 듣고 남은 시간
-//		Calendar planTimeBefore = Calendar.getInstance(); // 앞시간 수업
-//		Calendar planTimeAfter = Calendar.getInstance(); // 다음시간 수업
-//		for (int i = 0; i < plans.length; i++) {
-//			if (i < plans.length - 1) {
-//				String[] planTimeCompare1 = plans[i][1].split(":");
-//				String[] planTimeCompare2 = plans[i + 1][1].split(":");
-//
-//				planTimeBefore.set(2023, 11, 15, Integer.parseInt(planTimeCompare1[0]),Integer.parseInt(planTimeCompare1[1]));
-//				planTimeAfter.set(2023, 11, 15, Integer.parseInt(planTimeCompare2[0]),Integer.parseInt(planTimeCompare2[1]));
-//
-//				difference = (planTimeAfter.getTimeInMillis() - planTimeBefore.getTimeInMillis()) / 60000;
-//				planLeftTime = (int) (difference - Integer.parseInt(plans[i][2]));
-//				
-//				// 앞시간 수업이 다음시간 전에 끝날 경우
-//				if (planLeftTime >= 0) {
-//					planFinishSubject[finishCount] = plans[i][0];
-//					finishCount++;
-//				// 앞시간 수업이 다음시간 전에 끝나지 못하는 경우
-//				} else {
-//					planLeftSubject.put(plans[i][0], Math.abs(planLeftTime));
-//				}
-//				
-//				while (planLeftSubject.size() > 0 && planLeftTime > 0) {
-//					Entry<String, Integer> leftSubject = (Entry<String, Integer>) planLeftSubject.entrySet().toArray()[planLeftSubject.size() - 1];
-//					if (planLeftTime - leftSubject.getValue() >= 0) {
-//						planFinishSubject[finishCount] = leftSubject.getKey();
-//						finishCount++;
-//						planLeftTime -= leftSubject.getValue();
-//						planLeftSubject.remove(leftSubject.getKey());
-//					} else {
-//						planLeftTime -= leftSubject.getValue();
-//						planLeftSubject.put(leftSubject.getKey(), Math.abs(planLeftTime));
-//						break;
-//					}
-//				}
-//			} else {
-//				// 마지막 수업은 문제 없이 끝남
-//				planFinishSubject[finishCount] = plans[i][0];
-//				finishCount++;
-//
-//				while (planLeftSubject.size() > 0) {
-//					Entry<String, Integer> leftSubject = (Entry<String, Integer>) planLeftSubject.entrySet().toArray()[planLeftSubject.size() - 1];
-//					planFinishSubject[finishCount] = leftSubject.getKey();
-//					finishCount++;
-//					planLeftSubject.remove(leftSubject.getKey());
-//				}
-//			}
-//		}
-//		answer = planFinishSubject;
-//		return answer;
-//	}
-//}
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+/*
+과제는 시작하기로 한 시각이 되면 시작합니다.
+새로운 과제를 시작할 시각이 되었을 때, 
+
+기존에 진행 중이던 과제가 있다면 진행 중이던 과제를 멈추고 
+새로운 과제를 시작합니다.
+
+진행중이던 과제를 끝냈을 때, 잠시 멈춘 과제가 있다면, 
+멈춰둔 과제를 이어서 진행합니다.
+
+만약, 과제를 끝낸 시각에 새로 시작해야 되는 과제와 
+잠시 멈춰둔 과제가 모두 있다면, 새로 시작해야 하는 과제부터 진행합니다.
+
+멈춰둔 과제가 여러 개일 경우, 가장 최근에 멈춘 과제부터 시작합니다.
+plans	result
+[["korean", "11:40", "30"], ["english", "12:10", "20"], ["math", "12:30", "40"]]	["korean", "english", "math"]
+[["science", "12:40", "50"], ["music", "12:20", "40"], ["history", "14:00", "30"], ["computer", "12:30", "100"]]	["science", "history", "computer", "music"]
+[["aaa", "12:00", "20"], ["bbb", "12:10", "30"], ["ccc", "12:40", "10"]]	["bbb", "ccc", "aaa"]
+*/
+
+public class dangit3 {
+	public static void main(String[] args) {
+		int[][] scores = {
+				{ 85, 90 },
+				{ 65, 67 },
+				{ 88, 87 },
+				{ 88, 87 },
+				{ 73, 81 },
+				{ 65, 89 },
+				{ 99, 100 },
+				{ 80, 94 },
+		};
+		Solution ss = new Solution();
+		ss.solution(scores);
+	}
+}
+
+class Solution {
+	public int[] solution(int[][] scores) {
+        int[] answer = {};
+        
+        
+
+    	int[][] newScores = new int[scores.length][3];
+
+        System.out.println(scores.length);
+        int i=0;
+        int[] total = new int[scores.length];
+        int questionSum1 = 0;
+        int questionSum2 = 0;
+        for(int[] question : scores) {
+        	System.out.println("1번 문제 : " + question[0]);
+        	System.out.println("2번 문제 : " + question[1]);
+        	System.out.println("=========================");
+        	scores[i][2] = question[0] + question[1];
+        	questionSum1 += question[0];
+        	questionSum2 += question[1];
+        	i++;
+        }
+        
+        for(int[] question : scores) {
+        	System.out.println(question[2]);
+        }
+
+        return answer;
+	}
+}
